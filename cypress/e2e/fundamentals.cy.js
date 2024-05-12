a00e556ea7eff415ad2d27fb454258c4d67207cd
Bob <>
2024-05-12 17:30:40 +0300

    first commit h1 contains Testing Fundamentals

---
+++ cypress/e2e/fundamentals.cy.js
@@ -0,0 +1,6 @@
+describe('Fundamentals test', () => {
+  it('Contains correct header text', () => {
+    cy.visit('/fundamentals')
+    cy.get('[data-test="fundamentals-header"]').contains(/Testing Fundamentals/i)
+  })
+})
