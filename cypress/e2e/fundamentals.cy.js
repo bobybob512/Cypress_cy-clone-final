e6e541ed3aff79fb3e39fa450ec750b4172bf831
Bob <>
2024-05-12 18:39:27 +0300

    adding data-test and click on first item from Testing Fundamentals

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -3,4 +3,12 @@ describe('Fundamentals test', () => {
     cy.visit('/fundamentals')
     cy.get('[data-test="fundamentals-header"]').contains(/Testing Fundamentals/i)
   })
+  it('Accordion works correctly', () => {
+    cy.visit('/fundamentals')
+    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
+    cy.get('[data-test="accordion-item-1"]').click()
+    cy.contains(/Your tests will exist in a describe block/i).should('be.visible')
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
+    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
+  })
 })
